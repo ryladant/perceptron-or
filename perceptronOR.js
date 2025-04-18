342ebcfeb9ae6418ca610456fa576bd480e9c350
Ryladant <150343149+ryladant@users.noreply.github.com>
2025-04-18 10:59:03 -0300

    Update perceptronOR.js

--- perceptronOR.js
+++ perceptronOR.js
@@ -1,14 +1,14 @@
 inputs = [[0,0], [0,1], [1,0], [1,1]]
 outputs = [0,1,1,1]
 learning_rate = 0.1
-epochs = 1000
+epochs = 10000
 s = Math.random() > 0.5 ? -1 : 1 // aleatoriedade do sinal
 w1 = Math.random().toFixed(2) * s
 w2 = Math.random().toFixed(2) * s
 bias = Math.random().toFixed(2) * s
 
 for(let i=0; i < epochs; i++){
-	for(let j=0; j < inputs.length - 1; j++){
+	for(let j=0; j < inputs.length; j++){
     	x = (inputs[j][0] * w1 + inputs[j][1] * w2) + bias
     
     	sigmoid = 1 / (1 + Math.exp(-x))
@@ -22,11 +22,11 @@ for(let i=0; i < epochs; i++){
 
 function predict(a, b, c, x1, x2){
 	net = (x1 * w1) + (x2 * w2) + bias
-  
-	if(net >= 0) return 1
-  
-	return 0
+	output = 1 / (1 + Math.exp(-net))
+    alert(output)
+    if(output >= 0.5) return 1
+    return 0
 }
 
-prev = predict(w1, w1, bias, 0, 0)
+prev = predict(w1, w1, bias, 0, 1)
 alert(prev)
