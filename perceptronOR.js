feb1f4610cceb44976b4ffa2c6976cd13c669b9b
Ryladant <150343149+ryladant@users.noreply.github.com>
2025-04-19 18:53:25 -0300

    Update perceptronOR.js

--- perceptronOR.js
+++ perceptronOR.js
@@ -9,20 +9,20 @@ bias = Math.random().toFixed(2) * s
 
 for(let i=0; i < epochs; i++){
 	for(let j=0; j < inputs.length; j++){
-    	x = (inputs[j][0] * w1 + inputs[j][1] * w2) + bias
-    
-    	sigmoid = 1 / (1 + Math.exp(-x))
-        
-    	w1 = w1 + (0.1 * (outputs[j] - sigmoid) * inputs[j][0])
-		  w2 = w2 + (0.1 * (outputs[j] - sigmoid) * inputs[j][1])
-    
-      bias = bias + (0.1 * (outputs[j] - sigmoid))
+	x = (inputs[j][0] * w1 + inputs[j][1] * w2) + bias
+	
+	sigmoid = 1 / (1 + Math.exp(-x))
+	
+	w1 = w1 + (0.1 * (outputs[j] - sigmoid) * inputs[j][0])
+	w2 = w2 + (0.1 * (outputs[j] - sigmoid) * inputs[j][1])
+	
+	bias = bias + (0.1 * (outputs[j] - sigmoid))
     }
 }
 
 function predict(a, b, c, x1, x2){
-	net = (x1 * w1) + (x2 * w2) + bias
-	output = 1 / (1 + Math.exp(-net))
+    net = (x1 * w1) + (x2 * w2) + bias
+    output = 1 / (1 + Math.exp(-net))
     alert(output)
     if(output >= 0.5) return 1
     return 0
